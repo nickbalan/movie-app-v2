cbef67bf142a3a80f7cc4b99920c6748d7d5b64f
Nick Balan <nickolae.balan@gmail.com>
2022-11-29 00:09:36 +0100

    Updated the Welcome page and User Login/Registration dialog | v1.2

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-flix-angular-client'),
+      dir: require('path').join(__dirname, './coverage/movie-app-v2'),
       subdir: '.',
       reporters: [
         { type: 'html' },
